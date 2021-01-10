bf65a034a8bbf793c0e3f1981b40f11e0827e03e
tazz0009 <tazz0009@gmail.com>
2021-01-10 16:23:23 +0900

    first commit

--- README.md
+++ README.md
@@ -1,24 +1,61 @@
 # login_app
 
 ## Project setup
+
 ```
+vue create login_app
+
+? Please pick a preset: Manually select features
+? Check the features needed for your project: Choose Vue version, Babel, Router, Vuex, Linter
+? Choose a version of Vue.js that you want to start the project with 2.x
+? Use history mode for router? (Requires proper server setup for index fallback in production) Yes
+? Pick a linter / formatter config: Prettier
+? Pick additional lint features: Lint on save
+? Where do you prefer placing config for Babel, ESLint, etc.? In dedicated config files
+? Save this as a preset for future projects? No
+
+cd login_app
+
+vue add vuetify
+
+npm install material-design-icons-iconfont -D
+
+npm install @fortawesome/fontawesome-free -D
+
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
+### vuetify.js
+
+```
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+
+export default new Vuetify({
+  icons: {
+    iconfont: 'md',
+  },
+})
+```
+
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,150 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-container class="fill-height" fluid>
+    <v-row align="center" justify="center">
+      <v-col cols="12" sm="8" md="8">
+        <v-card class="elevation-12">
+          <v-window v-model="step">
+            <v-window-item :value="1">
+              <v-row>
+                <v-col cols="12" md="8">
+                  <v-card-text class="mt-12">
+                    <h1 class="text-center display-2 teal--text text--accent-3">
+                      Sign in to Diprella
+                    </h1>
+                    <div class="text-center mt-4">
+                      <v-btn class="mx-2" fab color="black" outlined>
+                        <v-icon>fab fa-facebook-f</v-icon>
+                      </v-btn>
+                      <v-btn class="mx-2" fab color="black" outlined>
+                        <v-icon>fab fa-google-plus-g</v-icon>
+                      </v-btn>
+                      <v-btn class="mx-2" fab color="black" outlined>
+                        <v-icon>fab fa-linkedin-in</v-icon>
+                      </v-btn>
+                    </div>
+                    <h4 class="text-center mlt-t">
+                      Ensure your email for registration
+                    </h4>
+                    <v-form>
+                      <v-text-field
+                        label="Email"
+                        name="Email"
+                        prepend-icon="email"
+                        type="text"
+                        color="teal accent-3"
+                      />
+                      <v-text-field
+                        id="password"
+                        label="Password"
+                        name="Password"
+                        prepend-icon="lock"
+                        type="password"
+                        color="teal accent-3"
+                      />
+                    </v-form>
+                    <h3 class="text-center mt-3">Forget your password?</h3>
+                  </v-card-text>
+                  <div class="text-center mt-3 mb-3">
+                    <v-btn rounded color="teal accent-3" dark>SIGN IN</v-btn>
+                  </div>
+                </v-col>
+                <v-col cols="12" md="4" class="teal accent-3">
+                  <v-card-text class="white--text mt-12">
+                    <h1 class="text-center display-1 ">Hello, Friends!</h1>
+                    <h5 class="text-center">
+                      Enter your personnel details and start journay with us
+                    </h5>
+                  </v-card-text>
+                  <div class="text-center mb-3">
+                    <v-btn rounded outlined dark @click="step++">SIGN UP</v-btn>
+                  </div>
+                </v-col>
+              </v-row>
+            </v-window-item>
+            <v-window-item :value="2">
+              <v-row class="fill-height">
+                <v-col cols="12" md="4" class="teal accent-3">
+                  <v-card-text class="white--text mt-12">
+                    <h1 class="text-center display-1">Welcom Back!</h1>
+                    <h5 class="text-center">
+                      To Keep connected with us please login with your personnel
+                      info
+                    </h5>
+                  </v-card-text>
+                  <div class="text-center mb-3">
+                    <v-btn rounded outlined dark @click="step--">
+                      SIGN IN
+                    </v-btn>
+                  </div>
+                </v-col>
+                <v-col cols="12" md="8">
+                  <v-card-text class="mt-12">
+                    <h1 class="text-center display-2 teal--text text--accent-3">
+                      Create Accont
+                    </h1>
+                    <div class="text-center mt-4">
+                      <v-btn class="mx-2" fab color="black" outlined>
+                        <v-icon>fab fa-facebook-f</v-icon>
+                      </v-btn>
+                      <v-btn class="mx-2" fab color="black" outlined>
+                        <v-icon>fab fa-google-plus-g</v-icon>
+                      </v-btn>
+                      <v-btn class="mx-2" fab color="black" outlined>
+                        <v-icon>fab fa-linkedin-in</v-icon>
+                      </v-btn>
+                    </div>
+                    <h4 class="text-center mt-4">
+                      Ensure your email for registration
+                    </h4>
+                    <v-form>
+                      <v-text-field
+                        label="Name"
+                        name="Name"
+                        prepend-icon="person"
+                        type="text"
+                        color="teal accent-3"
+                      />
+                      <v-text-field
+                        label="Email"
+                        name="Email"
+                        prepend-icon="email"
+                        type="text"
+                        color="teal accent-3"
+                      />
+                      <v-text-field
+                        id="password"
+                        label="Password"
+                        name="Password"
+                        prepend-icon="lock"
+                        type="password"
+                        color="teal accent-3"
+                      />
+                    </v-form>
+                  </v-card-text>
+                  <div class="text-center mb-3">
+                    <v-btn rounded color="teal accent-3" dark class="mb-3"
+                      >SIGN UP</v-btn
+                    >
+                  </div>
+                </v-col>
+              </v-row>
+            </v-window-item>
+          </v-window>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  components: {},
+  data: () => ({
+    step: 1
+  }),
+  props: {
+    source: String
   }
 };
 </script>
